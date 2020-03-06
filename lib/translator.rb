0fdce6a836334e172a0550f7284d181f93fb489e
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-03-06 09:59:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,6 +30,8 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticon_hash = load_library("./lib/emoticons.yml")
   
   if ((file_path = "./lib/emoticons.yml") && (emoticon = "=D"))
+    "(￣ー￣)" 
+  elsif 
   
 end
 
